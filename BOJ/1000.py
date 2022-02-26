45224f9c0d51caec51c4b546aea67ffb3b38dc46
lljuhee <75237512+lljuhee@users.noreply.github.com>
2022-02-26 19:37:09 +0900

    Add files via upload

---
+++ BOJ/1000.py
@@ -0,0 +1,23 @@
+"""
+문제
+두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
+
+출력
+첫째 줄에 A+B를 출력한다.
+"""
+
+A, B = input().split()
+print(int(A)+int(B))
+
+
+"""
+다른 답
+a, b = map(int, input().split())
+print(a+b)
+--------------------------
+map(function, iterable)
+iterable(리스트, 튜플 등)에 function 적용
+"""

9631e1598403c84f7512a3c40a6e7c9093331e27
Ju Hee <75237512+lljuhee@users.noreply.github.com>
2022-10-04 00:27:12 +0900

    Create 시각.py

---
+++ 이코테/구현/시각.py
@@ -0,0 +1,14 @@
+#00시 00분 00초부터 N시 59분 59초까지의 모든 시각 중에서 3이 하나라도 포함되는 모든 경우의 수
+
+# H를 입력받기
+h = int(input())
+
+count = 0
+for i in range(h + 1):
+    for j in range(60):
+        for k in range(60):
+            # 매 시각 안에 '3'이 포함되어 있다면 카운트 증가
+            if '3' in str(i) + str(j) + str(k):
+                count += 1
+
+print(count)

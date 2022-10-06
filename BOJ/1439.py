07a0549f34919ebb770932e7640cfb039665be36
Ju Hee <75237512+lljuhee@users.noreply.github.com>
2022-10-07 00:20:36 +0900

    Create 1439.py

---
+++ BOJ/1439.py
@@ -0,0 +1,23 @@
+'''
+입력 : 0001100
+출력 : 1
+'''
+
+data=input()
+
+count0 = 0
+count1 = 0
+
+for i in range(len(data)-1):
+  if data[i] != data[i+1]:
+    if data[i] == '1':
+      count0 += 1
+    else :
+      count1 += 1
+
+if data[i+1] == '1':
+  count0 +=1
+else :
+  count1 +=1 
+  
+print(min(count0,count1))
